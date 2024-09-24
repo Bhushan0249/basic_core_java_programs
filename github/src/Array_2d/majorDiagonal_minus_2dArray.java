b735d765e21f59ba4e13819f2e006c153f2170cb
Bhushan <bhushan0242@gmail.com>
2024-09-24 22:54:17 +0530

    print all elements above the major diagonal.

---
+++ github/src/Array_2d/majorDiagonal_minus_2dArray.java
@@ -0,0 +1,21 @@
+package Array_2d;
+
+public class majorDiagonal_minus_2dArray {
+// print all elements above the major diagonal.
+	public static void main(String[] args) {
+
+		int arr[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				if (i <= j) {
+					System.out.print(arr[i][j]+" ");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+}
